3e705f10627742f6eff97e5455aec7c758e84440
Валентин Ломов <help7@neox.ru>
2018-01-16 19:08:13 +0500

    ADD: Добавлена отправка уведомлений об изменениях в файлах сайта

--- Structure/Service/CheckSiteFiles/check_site_files.php
+++ Structure/Service/CheckSiteFiles/check_site_files.php
@@ -1,5 +1,7 @@
 <?php
 use Ideal\Structure\Service\CheckSiteFiles\Model as CheckSiteFilesModel;
+use Ideal\Core\Util;
+use Mail\Sender;
 
 $message = '';
 
@@ -31,16 +33,59 @@ if ($config->monitoring['excludedScanDir']) {
 if (!file_exists($checkFolder)) {
     $message = 'Указанной папки для сканирования не существует';
 } else {
+    $file = $siteFolder . '/tmp/site_hash_files';
     $cmsFolder = $siteFolder . '/' . $config->cmsFolder;
 
     // Собираем хэши файлов
-    $siteFiles = CheckSiteFilesModel::getAllSystemFiles($checkFolder, $siteFolder, $excluded);
+    $actualSiteFiles = CheckSiteFilesModel::getAllSystemFiles($checkFolder, $siteFolder, $excluded);
+
+    // Отправляем уведомление
+    if ($config->monitoring['excludedScanDir']) {
+        // Получаем хэши ранее собранных файлов
+        $siteFiles = unserialize(file_get_contents($file));
+
+        // Получаем разницу между свежими данными и ранее собранными
+        $diff = CheckSiteFilesModel::getDiff($actualSiteFiles, $siteFiles);
+
+        if ($diff['changeFiles']) {
+            $changeFiles = 'Были внесены изменения в следующие файлы:<br />';
+            $changeFiles .= implode('<br />', array_keys($diff['changeFiles'])) . '<br /><br />';
+        } else {
+            $changeFiles = 'Нет изменённых файлов<br /><br />';
+        }
+        if ($diff['delFiles']) {
+            $delFiles = 'Были удалены следующие файлы:<br />' . implode('<br />', array_keys($diff['delFiles']));
+        } else {
+            $delFiles = 'Нет удалённых файлов';
+        }
+        if ($diff['newFiles']) {
+            $newFiles = 'Были добавлены новые файлы:<br />' . implode('<br />', array_keys($diff['newFiles']));
+            $newFiles .= '<br /><br />';
+        } else {
+            $newFiles = 'Нет новых файлов<br /><br />';
+        }
+        $message = <<<EMAIL
+            <html>
+            <head></head>
+            <body>
+                {$newFiles}
+
+                {$changeFiles}
+    
+                {$delFiles}
+            </body>
+            </html>            
+EMAIL;
+        $mail = new Sender();
+        $mail->setSubj('Изменения файлов сайта "' . $config->domain . '"');
+        $mail->setHtmlBody($message);
+        $mail->sent($config->robotEmail, $config->monitoring['excludedScanDir']);
+    } else {
+        Util::addError('Не указан получатель уведомлений об изменениях файлов сайта');
+    }
 
     // Записываем данные в файл информации о хэшах файлов системы
-    $file = $siteFolder . '/tmp/site_hash_files';
-    if (file_put_contents($file, serialize($siteFiles))) {
-        echo "Success!\n";
-    } else {
-        echo "Write error in file {$file} \n";
+    if (!file_put_contents($file, serialize($actualSiteFiles))) {
+        $message = 'Не удалось записать данные о хэшах в файл "' . $file . '"';
     }
 }
